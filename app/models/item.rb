7e59739a9c5117edd52d49d901c23dfe231d7542
sho0612 <momonnga0612@gmail.com>
2024-06-01 18:10:12 +0900

    Activehashに対応したitemsモデルのバリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+
   belongs_to :user
 # has_one :order  
 
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_fee_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :shipping_day_id, presence: true
-
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
 
 end
