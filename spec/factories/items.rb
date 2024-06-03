455f9084a4de4f0fdce9f0c46e4a812d7ff1dc70
sho0612 <momonnga0612@gmail.com>
2024-06-04 00:30:46 +0900

    Rspecテストの準備

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,15 @@
 FactoryBot.define do
   factory :item do
-    name { "MyString" }
-    description { "MyText" }
-    price { 1 }
-    user { nil }
-    category_id { 1 }
-    condition_id { 1 }
-    shipping_fee_id { 1 }
-    prefecture_id { 1 }
-    shipping_day_id { 1 }
+    name { Faker::Commerce.product_name }
+    description { Faker::Lorem.paragraph }
+    price { Faker::Number.between(from: 300, to: 9999999) }
+    user
+    category_id { Faker::Number.between(from: 2, to: 10) }
+    condition_id { Faker::Number.between(from: 2, to: 6) }
+    shipping_fee_id { Faker::Number.between(from: 2, to: 4) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    shipping_day_id { Faker::Number.between(from: 2, to: 4) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/sample_image.png'), 'image/png') }
   end
 end
+
