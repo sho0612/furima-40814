d9800d8f7aafb1eb45c0e7adecb52baaaa5c0280
sho0612 <momonnga0612@gmail.com>
2024-06-04 17:27:11 +0900

    コントローラーの整理
    現段階で利用しないものをコメントアウト

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,12 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @items = Item.all
-  end
+  # def index
+  #   @items = Item.all
+  # end
 
-  def show
-  end
+  # def show
+  # end
 
   def new
     @item = current_user.items.build
@@ -22,21 +22,21 @@ class ItemsController < ApplicationController
     end
   end
 
-  def edit
-  end
+  # def edit
+  # end
 
-  def update
-    if @item.update(item_params)
-      redirect_to @item, notice: 'Item was successfully updated.'
-    else
-      render :edit
-    end
-  end
+  # def update
+  #   if @item.update(item_params)
+  #     redirect_to @item, notice: 'Item was successfully updated.'
+  #   else
+  #     render :edit
+  #   end
+  # end
 
-  def destroy
-    @item.destroy
-    redirect_to items_url, notice: 'Item was successfully destroyed.'
-  end
+  # def destroy
+  #   @item.destroy
+  #   redirect_to items_url, notice: 'Item was successfully destroyed.'
+  # end
 
   private
 
@@ -45,6 +45,7 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, :image)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
+                                 :shipping_day_id, :image)
   end
 end
