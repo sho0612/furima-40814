a551cffd0651d8c20e0984a04dc9050e8fdb2bf5
sho0612 <momonnga0612@gmail.com>
2024-06-10 19:58:22 +0900

    Payjp利用のためのモデルの修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,11 @@
 class Order < ApplicationRecord
+
+  attr_accessor :token
+
   belongs_to :user
   belongs_to :item
   has_one :address, dependent: :destroy
   accepts_nested_attributes_for :address
+
+
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,13 +1,13 @@
 # app/models/order_address.rb
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipalities, :street, :building, :phone_number, :user_id, :item_id
-
+  attr_accessor :post_code, :prefecture_id, :municipalities, :street, :building, :phone_number, :user_id, :item_id, :token
 
   validates :post_code, presence: true
   validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
   validates :prefecture_id, :municipalities, :street, :phone_number, presence: true
   validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid. Enter only 10 or 11 digits" }
+  validates :token, presence: true
 
   def save
     return false unless valid?
