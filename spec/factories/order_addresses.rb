c76bb16ff4d5144624628ae7fdb0a5532d25b3f3
sho0612 <momonnga0612@gmail.com>
2024-06-11 10:35:04 +0900

    Rspecテストを実装

---
+++ spec/factories/order_addresses.rb
@@ -0,0 +1,14 @@
+# spec/factories/order_addresses.rb
+FactoryBot.define do
+  factory :order_address do
+    post_code { '123-4567' }  # 固定のフォーマットを使用
+    prefecture_id { Faker::Number.between(from: 1, to: 47) }
+    municipalities { Faker::Address.city }
+    street { Faker::Address.street_address }
+    building { Faker::Address.secondary_address }
+    phone_number { Faker::Number.leading_zero_number(digits: 11) }
+    token { 'tok_' + Faker::Alphanumeric.alphanumeric(number: 24) }
+    user_id { Faker::Number.number(digits: 2) }
+    item_id { Faker::Number.number(digits: 2) }
+  end
+end
---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,82 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  before do
+    @order_address = FactoryBot.build(:order_address)
+  end
+
+  describe '商品購入機能' do
+    context '商品購入がうまくいくとき' do
+      it '全ての値が正しく入力されていれば保存できること' do
+        expect(@order_address).to be_valid
+      end
+
+      it 'buildingは空でも保存できること' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+    end
+
+    context '商品購入がうまくいかないとき' do
+      it 'post_codeが空だと保存できないこと' do
+        @order_address.post_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code can't be blank")
+      end
+
+      it 'post_codeが「3桁ハイフン4桁」の半角文字列でないと保存できないこと' do
+        @order_address.post_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Post code is invalid. Enter it as follows (e.g. 123-4567)")
+      end
+
+      it 'prefecture_idが空だと保存できないこと' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it 'municipalitiesが空だと保存できないこと' do
+        @order_address.municipalities = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Municipalities can't be blank")
+      end
+
+      it 'streetが空だと保存できないこと' do
+        @order_address.street = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Street can't be blank")
+      end
+
+      it 'phone_numberが空だと保存できないこと' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it 'phone_numberが10桁以上11桁以内の半角数値でないと保存できないこと' do
+        @order_address.phone_number = '090-1234-5678'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid. Enter only 10 or 11 digits")
+      end
+
+      it 'tokenが空だと保存できないこと' do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it 'user_idが空だと保存できないこと' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+
+      it 'item_idが空だと保存できないこと' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+    end
+  end
+end
