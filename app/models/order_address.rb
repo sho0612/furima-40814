cdeabb22d77ee3b419c5faf3f8a5473fb2e8b2a3
sho0612 <momonnga0612@gmail.com>
2024-06-11 15:55:32 +0900

    with_optionsでvalidationの記述を行う

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -3,11 +3,16 @@ class OrderAddress
   include ActiveModel::Model
   attr_accessor :post_code, :prefecture_id, :municipalities, :street, :building, :phone_number, :user_id, :item_id, :token
 
-  validates :post_code, presence: true
-  validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
-  validates :prefecture_id, :municipalities, :street, :phone_number, presence: true
-  validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter only 10 or 11 digits' }
-  validates :user_id, :item_id, :token, presence: true
+  with_options presence: true do
+    validates :post_code, format: { with: /\A\d{3}-\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :prefecture_id
+    validates :municipalities
+    validates :street
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Enter only 10 or 11 digits' }
+    validates :user_id
+    validates :item_id
+    validates :token
+  end
 
   def save
     return false unless valid?
